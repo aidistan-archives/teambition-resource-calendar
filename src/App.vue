ba16cce0b6aed6f9e26a84c719835c3822f44ccd
Aidi Stan <aidistan@live.com>
2016-07-30 01:29:07 +0800

    Implement basic functions

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,102 @@
-<template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-  </div>
+<template lang="pug">
+#app
+  #calendar(v-if="params.project")
+  h2(v-else, style="text-align: center") 未指定项目
 </template>
 
 <script>
-import Hello from './components/Hello'
+import qs from 'querystring'
+
+const name2rgb = {
+  red: '#ff5722',
+  yellow: '#ffc107',
+  green: '#bbc34a',
+  blue: '#03a9f4',
+  purple: '#ab47bc',
+  gray: '#a6a6a6'
+}
 
 export default {
-  components: {
-    Hello
+  data: function () {
+    return {
+      params: qs.parse(window.location.search.substr(1)),
+      resources: {},
+      events: []
+    }
+  },
+  ready: function () {
+    if (!this.params.project) return
+
+    this.$http({
+      url: `https://api.teambition.com/api/projects/${this.params.project}/tags`,
+      method: 'GET',
+      params: {
+        access_token: this.access_token
+      }
+    }).then((res) => {
+      for (let tag of res.json()) {
+        let name
+
+        if (/#[0-9a-f]{6}$/.test(tag.name)) {
+          name = tag.name.slice(0, -7)
+          this.resources[name] = {
+            eventColor: tag.name.slice(-7)
+          }
+        } else {
+          name = tag.name
+          this.resources[name] = {
+            eventColor: name2rgb[tag.color]
+          }
+        }
+
+        this.resources[name].id = name
+        this.resources[name].title = name
+      }
+
+      this.$http({
+        url: `https://api.teambition.com/api/projects/${this.params.project}/events`,
+        method: 'GET',
+        params: {
+          access_token: this.access_token
+        }
+      }).then((res) => {
+        for (let event of res.json()) {
+          if (this.resources[event.location]) {
+            this.events.push({
+              id: event._id,
+              resourceId: event.location,
+              title: event.title,
+              start: event.startDate
+            })
+          }
+        }
+
+        $('#calendar').fullCalendar({
+          // Appearances
+          height: 'auto',
+          lang: 'zh-cn',
+          timezone: 'local',
+
+          // View options
+          defaultView: 'agendaDay',
+          minTime: '08:00:00',
+          maxTime: '18:00:00',
+
+          // Data
+          events: this.events,
+          resources: Object.values(this.resources),
+
+          // GPL License
+          schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source'
+        })
+      })
+    })
   }
 }
 </script>
 
-<style>
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
+<style lang="scss">
 #app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
+  user-select: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App'
+import Resource from 'vue-resource'
+
+Vue.use(Resource)
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
+  components: { app: require('./App') }
 })
