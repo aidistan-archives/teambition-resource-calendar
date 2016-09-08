ad953631e5574d52403f96f27294588578145e7f
Aidi Stan <aidistan@live.com>
2016-09-09 00:36:12 +0800

    Add component AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template lang="pug">
-#app.container-fluid
-  router-view
+#app
+  app-header
+  .container-fluid
+    router-view
 </template>
 
 <script>
 // Import TB styles
 require('tb-styles/dist/styles/ui.min.css')
 require('tb-styles/dist/scripts/ui.min.js')
+
+export default {
+  components: {
+    'app-header': require('components/AppHeader')
+  }
+}
 </script>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.app-header
+  nav.navbar.navbar-default
+    ul.nav.navbar-nav
+      li(v-for="(name, displayName) in menuItems", :class="{ active: $route.name === name }")
+        a(v-link="{ name: name }") {{ displayName }}
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      menuItems: {
+        calendar: '资源日历',
+        conflicts: '冲突检测',
+        statistics: '利用统计'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.app-header {
+  ul.navbar-nav {
+    float: none;
+    text-align: center;
+
+    & > li {
+      float: none;
+      display: inline-block;
+    }
+  }
+}
+</style>
