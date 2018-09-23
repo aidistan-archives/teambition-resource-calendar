116176a232398152b1c8c06b470d9bcff153db3c
Aidi Stan <aidistan@live.com>
2018-09-23 23:09:16 +0800

    Support organization tags

--- src/store/actions.js
+++ src/store/actions.js
@@ -43,87 +43,21 @@ export function loadEventsAndResources ({ commit, state }) {
       url: `/v2/projects/${state.params.id}/members`,
       method: 'GET'
     })
-    .then((tbMembers) => {
-      members = Vue._(tbMembers).keyBy('_userId').mapValues((tbMember) => (
-        { id: tbMember._userId, title: tbMember.name, eventCount: 0 }
-      )).value()
-    })
+    .then(processMembers)
     .then(() => Vue.api({
       url: `/projects/${state.params.id}/tags`,
       method: 'GET'
     }))
-    .then((tags) => {
-      for (let tag of tags) {
-        let name = tag.name
-
-        ensureResource(tag._id, {
-          title: name,
-          _title: name,
-          eventColor: name2rgb[tag.color]
-        })
-
-        if (/#[0-9a-f]{6}$/.test(name)) {
-          resources[tag._id].title = name.slice(0, -7)
-          resources[tag._id].eventColor = name.slice(-7)
-        }
-      }
-    })
-    .then(() => {
-      let maxLevel = Vue._(resources)
-      .map((resource) => resource.title.split('::').length)
-      .max() || 1
-
-      resourceLevels = Vue._(Vue._.range(maxLevel)).map((i) => ({
-        group: i !== 0,
-        field: `level_${i}`
-      }))
-      .value().reverse()
-
-      for (let id in resources) {
-        let columns = resources[id].title.split('::')
-        resources[id]['level_0'] = columns.pop()
-
-        Vue._(Vue._.range(1, maxLevel)).each((i) => {
-          let col = columns[maxLevel - 1 - i]
-          resources[id][`level_${i}`] = col === undefined ? '' : col
-        })
-      }
-    })
+    .then(processTags)
     .then(() => Vue.api({
       url: `/projects/${state.params.id}/events?startDate=${Vue.m().subtract(1, 'year').toISOString()}`,
       method: 'GET'
     }))
     .then((tbEvents) => {
       if (tbEvents.length === 0) {
-        return Promise.reject(new Error('未找到任何日程，请在项目中新建'))
-      }
-
-      for (let tbEvent of tbEvents) {
-        let event = {
-          id: tbEvent._id,
-          title: tbEvent.title,
-          start: tbEvent.startDate,
-          end: tbEvent.endDate,
-          memberIds: Vue._.intersection(tbEvent.involveMembers, Vue._.keys(members)),
-          url: `https://www.teambition.com/project/${state.params.id}/events/event/${tbEvent._id}`
-        }
-
-        if (Vue._.isEmpty(tbEvent.tagIds)) {
-          if (tbEvent.location) {
-            ensureResource(tbEvent.location)
-            event.resourceIds = [tbEvent.location]
-          } else {
-            ensureResource('null', { title: '其他资源', level_0: '其他资源' })
-            event.resourceIds = ['null']
-          }
-        } else {
-          event.resourceIds = tbEvent.tagIds
-        }
-
-        for (let id of event.memberIds) members[id].eventCount += 1
-        for (let id of event.resourceIds) resources[id].eventCount += 1
-
-        events.push(event)
+        Promise.reject(new Error('未找到任何日程，请在项目中新建'))
+      } else {
+        processEvents(tbEvents)
       }
     })
   }
@@ -135,11 +69,7 @@ export function loadEventsAndResources ({ commit, state }) {
       url: `/v2/organizations/${state.params.id}/members`,
       method: 'GET'
     })
-    .then((tbMembers) => {
-      members = Vue._(tbMembers).keyBy('_userId').mapValues((tbMember) => (
-        { id: tbMember._userId, title: tbMember.name, eventCount: 0 }
-      )).value()
-    })
+    .then(processMembers)
     .then(() => Vue.api({
       url: `/organizations/${state.params.id}/teams`,
       method: 'GET'
@@ -150,38 +80,96 @@ export function loadEventsAndResources ({ commit, state }) {
       method: 'GET'
     }))
     .then((tbProjects) => {
-      return Promise.all(Vue._.map(tbProjects, (project) => {
-        return Vue.api({
-          url: `/projects/${project._id}/events?startDate=${Vue.m().subtract(1, 'year').toISOString()}`,
-          method: 'GET'
-        })
-        .then((tbEvents) => {
-          for (let tbEvent of tbEvents) {
-            let event = {
-              id: tbEvent._id,
-              title: tbEvent.title,
-              start: tbEvent.startDate,
-              end: tbEvent.endDate,
-              memberIds: Vue._.intersection(tbEvent.involveMembers, Vue._.keys(members)),
-              url: `https://www.teambition.com/project/${project._id}/events/event/${tbEvent._id}`
-            }
-
-            if (tbEvent.location) {
-              ensureResource(tbEvent.location)
-              event.resourceIds = [tbEvent.location]
-            } else {
-              ensureResource('null', { title: '其他资源', level_0: '其他资源' })
-              event.resourceIds = ['null']
-            }
-
-            for (let id of event.memberIds) members[id].eventCount += 1
-            for (let id of event.resourceIds) resources[id].eventCount += 1
-
-            events.push(event)
-          }
-        })
-      }))
+      return Promise.all(Vue._.map(tbProjects, (project) => Vue.api({
+        url: `/projects/${project._id}/tags`,
+        method: 'GET'
+      })))
+      .then((allTags) => Vue._(allTags)
+        .flatten().uniqBy('_id').filter('_organizationId').value())
+      .then(processTags)
+      .then(() => Promise.all(Vue._.map(tbProjects, (project) => Vue.api({
+        url: `/projects/${project._id}/events?startDate=${Vue.m().subtract(1, 'year').toISOString()}`,
+        method: 'GET'
+      }))))
+      .then((allEvents) => Vue._(allEvents)
+        .flatten().each((tbEvent) => {
+          tbEvent.tagIds = Vue._.intersection(tbEvent.tagIds, Vue._.keys(resources))
+        }))
+      .then(processEvents)
     })
+  }
+
+  function processMembers (tbMembers) {
+    members = Vue._(tbMembers).keyBy('_userId').mapValues((tbMember) => (
+      { id: tbMember._userId, title: tbMember.name, eventCount: 0 }
+    )).value()
+  }
+
+  function processTags (tags) {
+    for (let tag of tags) {
+      let name = tag.name
+
+      ensureResource(tag._id, {
+        title: name,
+        _title: name,
+        eventColor: name2rgb[tag.color]
+      })
+
+      if (/#[0-9a-f]{6}$/.test(name)) {
+        resources[tag._id].title = name.slice(0, -7)
+        resources[tag._id].eventColor = name.slice(-7)
+      }
+    }
+
+    let maxLevel = Vue._(resources)
+    .map((resource) => resource.title.split('::').length)
+    .max() || 1
+
+    resourceLevels = Vue._(Vue._.range(maxLevel)).map((i) => ({
+      group: i !== 0,
+      field: `level_${i}`
+    }))
+    .value().reverse()
+
+    for (let id in resources) {
+      let columns = resources[id].title.split('::')
+      resources[id]['level_0'] = columns.pop()
+
+      Vue._(Vue._.range(1, maxLevel)).each((i) => {
+        let col = columns[maxLevel - 1 - i]
+        resources[id][`level_${i}`] = col === undefined ? '' : col
+      })
+    }
+  }
+
+  function processEvents (tbEvents) {
+    for (let tbEvent of tbEvents) {
+      let event = {
+        id: tbEvent._id,
+        title: tbEvent.title,
+        start: tbEvent.startDate,
+        end: tbEvent.endDate,
+        memberIds: Vue._.intersection(tbEvent.involveMembers, Vue._.keys(members)),
+        url: `https://www.teambition.com/project/${state.params.id}/events/event/${tbEvent._id}`
+      }
+
+      if (Vue._.isEmpty(tbEvent.tagIds)) {
+        if (tbEvent.location) {
+          ensureResource(tbEvent.location)
+          event.resourceIds = [tbEvent.location]
+        } else {
+          ensureResource('null', { title: '其他资源', level_0: '其他资源' })
+          event.resourceIds = ['null']
+        }
+      } else {
+        event.resourceIds = tbEvent.tagIds
+      }
+
+      for (let id of event.memberIds) members[id].eventCount += 1
+      for (let id of event.resourceIds) resources[id].eventCount += 1
+
+      events.push(event)
+    }
   }
 
   function ensureResource (id, resource) {
